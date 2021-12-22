b4e8529e4f78433479330b87ced6b60d488ae1e9
yang91127 <10160916@ms1.mcu.edu.tw>
2021-12-22 20:38:15 +0800

    加入week14

---
+++ week14/week14-4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int a[100][100];
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            scanf("%d", &b[i][j] );
+        }
+    }
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            c[i][j] = a[i][j]+b[i][j];
+            printf("%3d ", c[i][j] );
+        }
+        printf("\n");
+
+
+    }
+}
